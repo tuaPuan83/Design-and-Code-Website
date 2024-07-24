670231fe83e5b59cc9f38e650e031d00c1bb4820
William Klaiwonpen <williamklaiwonpen@gmail.com>
2024-07-24 20:48:34 +0700

    Mobile Navbar

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const toggleButton = document.querySelector('.navbar__mobile-menu-toggle');
-    const mobileMenu = document.querySelector('.navbar__mobile-menu-items');
-
-    toggleButton.addEventListener('click', function() {
-        mobileMenu.classList.toggle('active')
-    })
+    
 })
