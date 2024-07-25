7a93e96b8f39a4ca5dd84f0d2f3795f81aecaeba
William Klaiwonpen <williamklaiwonpen@gmail.com>
2024-07-25 10:42:45 +0700

    Hero Section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.querySelector('.navbar .mobile-menu-toggle');
+
+    const mobileMenu = document.querySelector('.navbar .mobile-menu-items');
+
+    toggleButton.addEventListener('click', function() {
+        mobileMenu.classList.toggle('active');
+    })
+
     
 })
